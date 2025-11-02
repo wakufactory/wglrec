544fc4f32354f5a52c4d00054ed230fc8bcb9e39
wakufactory <waku@lang.co.jp>
2025-11-02 16:04:58 +0900

    add settings string

--- path3.js
+++ path3.js
@@ -1,10 +1,18 @@
 // path.js
 // path tracing scene
-export {createSceneController} from './shader_mod.js' ;
+export {createSceneController} from './shader_mod.js?' ;
 //settings
 globalThis.shader_settings = {
+  SETTINGS:`
+    const int MAX_BOUNCES = 4;
+    const int SPP = 4; // samples per pixel
+    const float STEREO = 0. ;       // stereo ipd 
+    const bool STEREO_TARGET = true ; //stereo target   
+    const int RANDOM_SEED = 1 ; //random noize seed par frame
+ 
+    const int HIDDEN_LIGHT = 0 ;    //invisible mirror 
+  `,
   SHADER_CHUNK_FILES : [
-  'path2-setting.glsl',
   'path2-define.glsl',
   'path2-material.glsl',
   'path3-model.glsl',
@@ -12,4 +20,3 @@ globalThis.shader_settings = {
   'path2-main.glsl'
   ]
 }
-
