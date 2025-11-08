251840c21cd82c1e7e60196a934d782e82e5a1be
wakufactory <waku@lang.co.jp>
2025-11-08 17:09:25 +0900

    remove works

--- works/chunk/path2-setting.glsl
+++
@@ -1,8 +0,0 @@
-// render settings
-const int MAX_BOUNCES = 4;
-const int SPP = 4; // samples per pixel
-const float STEREO = 0. ;       // stereo ipd 
-const bool STEREO_TARGET = true ; //stereo target   
-const int RANDOM_SEED = 1 ; //random noize seed par frame
- 
- const int HIDDEN_LIGHT = 0 ;    //invisible mirror 
